f8d7809a030e17aacb98f65eab21e6bcded765e1
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-12-23 13:57:31 -0500

    added sequelize helper methods

--- models/band.js
+++ models/band.js
@@ -1,40 +1,42 @@
-// DEPENDENCIES
-const { Sequelize, DataTypes,  Model } = require('sequelize')
-// const sequelize = new Sequelize(process.env.PG_URI)
-
-// MODEL
-class Band extends Model{}
-
-
-Band.init({
-    band_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    genre: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    available_start_time: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    end_time: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+   
+    static associate(models) {
+      
+    }
+  }
+  Band.init({
+      band_id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      },
+      genre: {
+          type: DataTypes.TEXT,
+          allowNull: false
+      },
+      available_start_time: {
+          type: DataTypes.DATE,
+          allowNull: false
+      },
+      end_time: {
+          type: DataTypes.DATE,
+          allowNull: false
+      },
   }, {
-    sequelize,                           
-    modelName: 'Band',
-    tableName: 'bands',
-    timestamps: false
-
-
-})
-// EXPORT
-module.exports = Band
+    sequelize,
+      modelName: 'Band',
+      tableName: 'bands',
+      timestamps: false
+      
+  });
+  return Band;
+};
